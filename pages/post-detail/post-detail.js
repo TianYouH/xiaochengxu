64784935f09f0cae5c9ef477c1c0c011a930de4d
LAPTOP-CRCIOU48\hjl <1406957473@qq.com>
2020-12-30 20:29:24 +0800

    feat: 完成新闻详情页开发

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -1,5 +1,7 @@
 // pages/post-detail.js
-import { postList } from "../../data/posts-data"
+import {
+  postList
+} from "../../data/posts-data"
 const app = getApp();
 console.log('app:', app)
 Page({
@@ -10,40 +12,49 @@ Page({
   data: {
     postData: {},
     collected: false,
-    _pid: null
+    _pid: null,
+    _postsCollected: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const { pid } = options;
-    const collected = wx.getStorageSync('posts_collected')[pid];
+    const {
+      pid
+    } = options;
+    const _postsCollected = wx.getStorageSync('posts_collected');
+    const collected = _postsCollected[pid];
     const postData = postList.find(post => (post.postId === Number(pid)));
     // console.log("文章数据：", postData);
     this.setData({
       postData,
       _pid: pid,
-      collected
+      collected,
+      _postsCollected
     })
   },
 
   onCollect(event) {
-    if (this.data.collected) {
-      wx.setStorageSync('posts_collected', {
-        [this.data._pid]: false
-      })
+    const posts_collected = this.data._postsCollected;
+      posts_collected[this.data._pid] = !this.data.collected;
+      wx.setStorageSync('posts_collected', posts_collected)
+      // this.data.collected = !this.data.collected; // 不会触发数据绑定
       this.setData({
-        collected: false
+        collected: !this.data.collected
       })
-      return;
-    }
-    wx.setStorageSync('posts_collected', {
-      [this.data._pid]: true
+      wx.showToast({
+        title: this.data.collected ? '收藏成功':'取消收藏',
+        icon: 'success',
+        duration: 2000
+      })
+  },
+
+  async onShare() {
+    const result = await wx.showActionSheet({
+      itemList: ['分享到空间', '你好世界', '666'],
     })
-    this.setData({
-      collected: true
-    })
+    console.log('点击结果：', result);
   },
 
   /**
